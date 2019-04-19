3d41feff02c15e45193da1d2328ee77ca342d9a2
Linnea Strand <gusstrlip@student.gu.se>
2019-04-19 16:13:19 +0200

    Add start- and endline functionality

--- gendata.py
+++ gendata.py
@@ -1,13 +1,57 @@
 import os, sys, glob, argparse, numpy as np, pandas as pd, subprocess, re
 from nltk import word_tokenize
 
+'''I removed metavars from R and P because I saw no use of them
+and they were causing conflict
+'''
+
 #Part 1: Preprocessing
 
-def process_args():
+def retrieving_data(filename):
+    ''' to do:
+            - percentage of set to be used training/test
+            - further tokenisation
+            - truncation 
+    '''
+    language_text = []
 
-    ''' Here we will add the parsers to the program so we can save them as 
-    variables'''
+    with open(filename, encoding ='UTF-8') as f:
+        position = 0 #keep track of reading position
+        if args.startline:
+            print('Starting at line {}.'.format(args.startline))
+            for i in range(args.startline): # start at line "startline"
+                position += 1
+                line = f.readline()
+        for line in f:
+            position += 1
+            line = word_tokenize(line) #tolower?
+            language_text.append(line)
+            if position == args.endline:
+                break
 
+    return language_text
+
+    print('Length of English lines: {}'.format(len(english_lines)))
+    print('Length of French lines: {}'.format(len(french_lines)))
+
+def truncate_me(text1, text2):
+    '''Zip languages together --Lin
+    this returns a list of tuples that is the french/english line
+    i have truncated it, i don't know if you think there is a better way to output 
+    this bit? --Rob
+    '''
+    twin_lines = []
+    for i in range(len(text1)):
+        lens = [len(text1[i]), len(text2[i])]
+        print(lens)
+        twin_lines.append((text1[i][:min(lens)],text2[i][:min(lens)]))
+        #print(len(twin_lines[i][0]), len(twin_lines[i][1])) #testing that it worked
+    return twin_lines 
+
+def feed_forward():
+    pass
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Convert text to features")
     parser.add_argument("-S", "--start", metavar="S", dest="startline", type=int,
                     default=0,
@@ -17,52 +61,11 @@ def process_args():
                     help="What line of the input data file to end on. Default is None, whatever the last line is.")
     parser.add_argument("-T", "--test", metavar="T", dest="test_range",
                     type=int, default=20, help="What percentage of the set will be test")
-    parser.add_argument("-R", "--random", metavar="R", dest="random", action="store_true", default=False, help="Specify whether to get random training/test")
-    parser.add_argument("-P", "--preprocessing", metavar="P", dest="prepro", action="store_true", default=False,
+    parser.add_argument("-R", "--random", dest="random", action="store_true", default=False, help="Specify whether to get random training/test")
+    parser.add_argument("-P", "--preprocessing", dest="prepro", action="store_true", default=False,
                         help="specifies whether or not to use preprocessing")
     args = parser.parse_args()
-    
-    return args.startline, args.endline, args.test_range, args.random, args.prepro
 
-def retrieving_data(english, french):
-
-    ''' to do:
-            - start/endline functionality
-            - percentage of set to be used training/test
-            - further tokenisation
-            - truncation 
-    '''
-
-    french_lines = []
-    with open(english, encoding ='UTF-8') as en:
-        
-        #room here for further tokenisation, we can discuss what we should use
-        english_lines = [word_tokenize(i) for i in en]
-        '''
-        for i in en:
-            #line = i.split(' ')
-            line = word_tokenize(i)
-            lines.append(line)
-        '''
-        en.close()
-    with open(french, encoding="UTF-8") as fr:
-        french_lines = [word_tokenize(i) for i in fr]
-        fr.close()
-
-    twin_lines = []
-    for i in range(len(english_lines)):
-        lens = [len(english_lines[i]), len(french_lines[i])]
-        print(lens)
-        twin_lines.append((english_lines[i][:min(lens)],french_lines[i][:min(lens)]))
-        #print(len(twin_lines[i][0]), len(twin_lines[i][1])) #testing that it worked
-
-    return twin_lines '''this returns a list of tuples that is the french/english line
-                        i have truncated it, i don't know if you think there is a better way to output 
-                        this bit?'''
-                          
-#Part 2: Vectorisation
-
-
-if __name__ == '__main__':
-    #retrieving_data('french_slice.txt')
-    retrieving_data('english_slice.txt', 'french_slice.txt')
+    english_lines = retrieving_data('english_slice.txt')
+    french_lines = retrieving_data('french_slice.txt')
+    truncate_me(english_lines,french_lines)
