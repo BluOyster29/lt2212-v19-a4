c8fdd1ca6b54f5405b3440d886a485e9ef2ff577
Linnea Strand <gusstrlip@student.gu.se>
2019-04-20 13:20:23 +0200

    add test/train split. consider removing stopwords

--- gendata.py
+++ gendata.py
@@ -1,5 +1,8 @@
 import os, sys, glob, argparse, numpy as np, pandas as pd, subprocess, re
 from nltk import word_tokenize
+from nltk.corpus import stopwords
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 '''I removed metavars from R and P because I saw no use of them
 and they were causing conflict
@@ -7,16 +10,23 @@ and they were causing conflict
 
 #Part 1: Preprocessing
 
-def retrieving_data(filename):
-    ''' to do:
-            - percentage of set to be used training/test
-            - further tokenisation
-            - truncation 
+def retrieving_data(filename, language_name):
+    '''to do:
+        - percentage of set to be used training/test
+        - further tokenisation
+        - truncation 
+
+    Args:
+        filename: .txt file of source/target language data
+        language_name: string
+    Output:
+        language_text: list: tokenized data without stopwords
     '''
     language_text = []
+    stop_words = stopwords.words(language_name)
 
     with open(filename, encoding ='UTF-8') as f:
-        position = 0 #keep track of reading position
+        position = 0 # to keep track of reading position
         if args.startline:
             print('Starting at line {}.'.format(args.startline))
             for i in range(args.startline): # start at line "startline"
@@ -24,7 +34,7 @@ def retrieving_data(filename):
                 line = f.readline()
         for line in f:
             position += 1
-            line = word_tokenize(line) #tolower?
+            line = word_tokenize(line) # tolower?
             language_text.append(line)
             if position == args.endline:
                 break
@@ -48,8 +58,23 @@ def truncate_me(text1, text2):
         #print(len(twin_lines[i][0]), len(twin_lines[i][1])) #testing that it worked
     return twin_lines 
 
-def feed_forward():
-    pass
+def gengrams(text):
+    trigrams = ngrams(text, 3, pad_left=True, pad_right=False, left_pad_symbol='<s>')
+    return trigrams
+
+def split_data(data, T):
+    '''To split our test and training. We could also do this manually if you prefer.
+    Let's figure out what the data is supposed to look like first ;)
+    Args:
+        data: arrays
+        T: desired size of test data in fractions (e.g. 0.1, 0.4...)
+    Output:
+        arrays
+    '''
+
+    X_train, X_test, y_train, y_test = train_test_split(data, test_size=T) #should we shuffle? yes/no
+
+    return X_train, X_test, y_train, y_test
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Convert text to features")
@@ -66,6 +91,6 @@ if __name__ == '__main__':
                         help="specifies whether or not to use preprocessing")
     args = parser.parse_args()
 
-    english_lines = retrieving_data('english_slice.txt')
-    french_lines = retrieving_data('french_slice.txt')
+    english_lines = retrieving_data('english_slice.txt', 'english')
+    french_lines = retrieving_data('french_slice.txt', 'french')
     truncate_me(english_lines,french_lines)
